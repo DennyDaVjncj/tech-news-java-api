1d0e685a59863f604ae08bd81b5dc4e29ba37c21
Denny da Vjncj <daVjncjCode@gmail.com>
2021-09-18 14:56:48 -0400

    playing with classes and variables

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,9 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args){
+        int lottery=22346;
+        double expenditures=7996.99;
+        long sum=lottery+expenditures;
+    }
+}
