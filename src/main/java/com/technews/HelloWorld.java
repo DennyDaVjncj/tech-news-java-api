4331b8db9d5034849312b5e95680b11c96e541a9
Denny da Vjncj <daVjncjCode@gmail.com>
2021-09-21 08:51:33 -0400

    Demo test model

--- src/main/java/com/technews/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.technews;
-
-public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Ca va, ami!");
-        System.out.println("I actually did it, I wrote my first Java code!");
-    }
-}
