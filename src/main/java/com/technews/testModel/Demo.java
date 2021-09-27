a324020486390695702aa68746ec8f7f727256f4
Denny da Vjncj <daVjncjCode@gmail.com>
2021-09-27 08:29:27 -0400

    overwritting default functionality

--- src/main/java/com/technews/testModel/Demo.java
+++ src/main/java/com/technews/testModel/Demo.java
@@ -1,5 +1,7 @@
 package com.technews.testModel;
 
+import java.util.Objects;
+
 public class Demo {
     private String name;
     private int age;
@@ -25,4 +27,16 @@ public class Demo {
         this.age = age;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Demo demo = (Demo) o;
+        return age == demo.age && Objects.equals(name, demo.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
 }
