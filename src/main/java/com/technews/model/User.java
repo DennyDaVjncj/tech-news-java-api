8cc9aee232b410e83eea402c8b04fed535346b50
Denny da Vjncj <daVjncjCode@gmail.com>
2021-10-08 09:51:10 -0400

    table relationships, methods and annotations

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -1,12 +1,16 @@
 package com.technews.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
 
 @Enity
 @JsonIgnoreProperites({"hibernateLazyInitializer","handler"})
 @Table(name="user")
-public class User {
+public class User implements Serializable{
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
     private String username;
     @Column(unique=true)
@@ -14,8 +18,73 @@ public class User {
     private String password;
     @Transient
     private Boolean loggedIn;
+
     @OneToMany(mappedBy="userId",cascade=CascadeType.ALL,fetch=FetchType.EAGER)
     private List<Post> posts;
-    private List<vote> votes;
+    @OneToMany(mappedBy="userId",cascade=CascadeType.ALL,fetch=FetchType.LAZY)
+    private List<Vote> votes;
+    @OneToMany(mappedBy="userId",cascade=CascadeType.ALL,fetch=FetchType.LAZY)
     private List<Comment> comments;
+
+    public User(){
+    }
+    public User(Integer id,String username,String email,String password){
+        this.id=id;
+        this.username=username;
+        this.email=email;
+        this.password=password;
+    }
+    public Integer getId(){
+        return id;
+    }
+    public void setId(Integer id){
+        this.id=id;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public void setUsername(String username){
+        this.username=username;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    public boolean isLoggedIn(){
+        return loggedIn;
+    }
+    public void setLoggedIn(boolean loggedIn){
+        this.loggedIn=loggedIn;
+    }
+
+    public List<Post> getPosts(){
+        return posts;
+    }
+    public void setPosts(List<Post> posts){
+        this.posts=posts;
+    }
+    public List<Vote> getVotes(){
+        return votes;
+    }
+    public void setVotes(List<Vote> votes){
+        this.votes=votes;
+    }
+    public List<Comment> getComments(){
+        return comments;
+    }
+    public void setComments(List<Comment> comments){
+        this.comments=comments;
+    }
+    @Override
+    public boolean equals(Object o){
+        if(this==)
+    }
 }
