da5ffa1752c5717bcfc40215df37ef8d6a548607
Denny da Vjncj <daVjncjCode@gmail.com>
2021-10-08 00:30:28 -0400

    class level & instanceLevel annotaions

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -1,10 +1,21 @@
 package com.technews.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+
+import javax.persistence.*;
 
 @Enity
 @JsonIgnoreProperites({"hibernateLazyInitializer","handler"})
 @Table(name="user")
 public class User {
+    private Integer id;
+    private String username;
+    @Column(unique=true)
+    private String email;
+    private String password;
+    @Transient
+    private Boolean loggedIn;
+    @OneToMany(mappedBy="userId",cascade=CascadeType.ALL,fetch=FetchType.EAGER)
+    private List<Post> posts;
+    private List<vote> votes;
+    private List<Comment> comments;
 }
