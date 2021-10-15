ea11fe33ae419b74e09586d828648962d3204214
Denny da Vjncj <daVjncjCode@gmail.com>
2021-10-14 20:06:40 -0400

    newly added instance fields to the post class

--- src/main/java/com/technews/model/Post.java
+++ src/main/java/com/technews/model/Post.java
@@ -1,4 +1,10 @@
 package com.technews.model;
 
 public class Post {
+    private Integer id;
+    private String title;
+    private String postUrl;
+    private String userName;
+    private int voteCount;
+    private Integer userId;
 }
