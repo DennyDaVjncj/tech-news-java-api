ab589a3990936cf8742c2ca47bc9feb42a37f865
Denny da Vjncj <daVjncjCode@gmail.com>
2021-10-16 10:02:16 -0400

    instance level annotations added

--- src/main/java/com/technews/model/Post.java
+++ src/main/java/com/technews/model/Post.java
@@ -1,20 +1,31 @@
 package com.technews.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 @Table(name="post")
 public class Post {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
     private String title;
     private String postUrl;
+    @Transient
     private String userName;
+    @Transient
     private int voteCount;
     private Integer userId;
+    @NotNull
+    @Temporal(TemporalType.Date)
+    @Column(name="posted_at ")
     private Date postedAt=new Date();
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    @Column(name="updated_at ")
     private Date updatedAt=new Date();
     private List<Comment> comments;
 }
