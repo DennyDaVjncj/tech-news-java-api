7b3993e17a990a17be75e3ab54f7c106dcb80620
Denny da Vjncj <daVjncjCode@gmail.com>
2021-10-22 11:49:35 -0400

    annotations, constructor and get/set methods

--- src/main/java/com/technews/model/Post.java
+++ src/main/java/com/technews/model/Post.java
@@ -1,12 +1,17 @@
 package com.technews.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.Sun.istack.NotNull;
 import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 @Table(name="post")
-public class Post {
+public class Post implements Serializable{
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
@@ -28,5 +33,71 @@ public class Post {
     @Temporal(TemporalType.DATE)
     @Column(name="updated_at ")
     private Date updatedAt=new Date();
+
+    @OneToMany(mappedBy="postId",cascade=CascadeType.All,fetch=FetchType.LAZY)
     private List<Comment> comments;
+
+    public Post(){
+    }
+    public Post(Integer id,String title,String postUrl,int voteCount,Integer userId){
+        this.id=id;
+        this.title=title;
+        this.postUrl=postUrl;
+        this.voteCount=voteCount;
+        this.userId=userId;
+    }
+    public Integer getId(){
+        return id;
+    }
+    public void setId(Integer id){
+        this.id = id;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public String getPostUrl(){
+        return postUrl;
+    }
+    public void setPostUrl(String postUrl) {
+        this.postUrl = postUrl;
+    }
+    public String getUserName(){
+        return userName;
+    }
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+    public int getVoteCount(){
+        return voteCount;
+    }
+    public void setVoteCount(int voteCount){
+        this.voteCount = voteCount;
+    }
+    public Integer getUserId(){
+        return userId;
+    }
+    public void setUserId(Integer userId){
+        this.userId = userId;
+    }
+    public Date getPostedAt(){
+        return postedAt;
+    }
+    public void setPostedAt(Date postedAt){
+        this.postedAt = postedAt;
+    }
+    public Date getUpdatedAt(){
+        return updatedAt;
+    }
+    public void setUpdatedAt(Date updatedAt){
+        this.updatedAt = updatedAt;
+    }
+    public List<Comment> getComments(){
+        return comments;
+    }
+    public void setComments(List<Comment> comments){
+        this.comments = comments;
+    }
 }
