40979684f7fc5d5ca707faa22eb03d79ec2ca8f2
Denny da Vjncj <daVjncjCode@gmail.com>
2021-11-27 21:21:12 -0500

    constructor&getter/setter methods added

--- src/main/java/com/technews/model/Comment.java
+++ src/main/java/com/technews/model/Comment.java
@@ -1,7 +1,8 @@
 package com.technews.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
@@ -13,4 +14,40 @@ public class Comment {
     private String commentText;
     private Integer userId;
     private Integer postId;//all these iVariables being class level
+
+    public Comment(){}
+    public Comment(Integer id,String commentText,Integer userId,Integer postId){
+        this.id=id;
+        this.commentText=commentText;
+        this.userId=userId;
+        this.postId=postId;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getCommentText() {
+        return commentText;
+    }
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
 }
