be2f821bd0944c2fedc580095c9ac910de45f46a
Denny da Vjncj <daVjncjCode@gmail.com>
2021-12-01 21:13:35 -0500

    constructors configured

--- src/main/java/com/technews/model/Vote.java
+++ src/main/java/com/technews/model/Vote.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import java.util.Objects;//figure out what these imports are about & when they're invoked
 
 @Entity
-@JsonIgnoreProperties({"hibernateeLazyInitializer","handler"})
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
 @Table(name="vote")
 public class Vote implements Serializable{
     @Id
@@ -13,4 +13,14 @@ public class Vote implements Serializable{
     private Integer id;
     private Integer userId;
     private Integer postId;
+    //instance fields above
+
+    public Vote(){}//what is this method for?
+    //constructor method below
+    public Vote(Integer id,Integer userId,Integer postId){
+        this.id=id;
+        this.userId=userId;
+        this.postId=postId;
+    }
 }
+
