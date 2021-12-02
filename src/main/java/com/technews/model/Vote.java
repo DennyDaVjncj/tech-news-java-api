d2840adfc62bd4c2dd06aff3dc8b54220cf0b23b
Denny da Vjncj <daVjncjCode@gmail.com>
2021-12-01 19:48:50 -0500

    class level annotations imported

--- src/main/java/com/technews/model/Vote.java
+++ src/main/java/com/technews/model/Vote.java
@@ -1,6 +1,12 @@
 package com.technews.model;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
-public class Vote {
+@Entity
+@JsonIgnoreProperties({"hibernateeLazyInitializer","handler"})
+@Table
+public class Vote{
     private Integer id;
     private Integer userId;
     private Integer postId;
