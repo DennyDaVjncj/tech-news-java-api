b7441dc0dffda14bb0c1a89741430a6f21b19acb
Denny da Vjncj <daVjncjCode@gmail.com>
2021-12-16 08:54:34 -0500

    newly configured interface

---
+++ src/main/java/com/technews/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.technews.repository;
+
+//classes were essentially blueprints, where interfaces are more like 'contracts'
+//whatever class implements an interface, it must abide by all terms of contract, it must host all methods within interface
+
+public interface UserRepository {
+}
