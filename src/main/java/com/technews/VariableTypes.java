51a5c55c3aef9cb7022cfde134c68e8b3fe88bec
Denny da Vjncj <daVjncjCode@gmail.com>
2021-12-17 11:21:25 -0500

    version control

--- src/main/java/com/technews/VariableTypes.java
+++
@@ -1,7 +0,0 @@
-package com.technews;
-
-public class VariableTypes {
-    public static void main(String[] args){
-        System.out.println("I wish these instructions were written better...");
-    }
-}
