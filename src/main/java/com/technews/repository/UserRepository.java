4fd9df973eb11ef94a51b72334ec5b51a2968d22
Denny da Vjncj <daVjncjCode@gmail.com>
2021-12-17 11:02:45 -0500

    version control

--- src/main/java/com/technews/repository/UserRepository.java
+++ src/main/java/com/technews/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package com.technews.repository;
-
-//classes were essentially blueprints, where interfaces are more like 'contracts'
-//whatever class implements an interface, it must abide by all terms of contract, it must host all methods within interface
-//a Data Access Object (DAO) in Java is any class that retrieves, stores and searches data
-
 import com.technews.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+//classes were essentially blueprints, where interfaces are more like 'contracts'
+//whatever class implements an interface, it must abide by all terms of contract, it must host all methods within interface
+//a Data Access Object (DAO) in Java is any class that retrieves, stores and searches data
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
