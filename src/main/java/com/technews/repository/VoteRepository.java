d29d12cd80763e364d89993c7eb5740c1a01678e
Denny da Vjncj <daVjncjCode@gmail.com>
2021-12-18 12:02:21 -0500

    successful launch!

--- src/main/java/com/technews/repository/VoteRepository.java
+++ src/main/java/com/technews/repository/VoteRepository.java
@@ -8,3 +8,4 @@ public interface VoteRepository extends JpaRepository<Vote,Integer>{
     @Query("SELECT count(*) FROM Vote v where v.postId=:id")
     int countVotesByPostId(@Param("id") Integer id);
 }
+//successfully launched app!
