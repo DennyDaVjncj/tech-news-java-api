59015c261efb9c1be261e8d129a949f9bad5debf
Denny da Vjncj <daVjncjCode@gmail.com>
2021-12-22 13:43:13 -0500

    tentative controller configuration

--- src/main/java/com/technews/controller/UserController.java
+++ src/main/java/com/technews/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.technews.controller;
 
+import com.technews.model.Post;
+import com.technews.model.User;
 import com.technews.repository.UserRepository;
 import com.technews.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -16,6 +21,21 @@ public class UserController {
     @GetMapping("/api/users")
     public List<User> getAllUsers(){
         List<User> userList=Repository.findAll();
+        for (User u:userList){
+            List<Post>postList=u.getPosts();
+            for(Post p:postList){
+                p.setVoteCount(voteRepository.countVotesByPostId(p.getId()));
+            }
+        }
+        return userList;
     }
-
+    @GetMapping("/api/users/{id}")
+    public User getUserById(@PathVariable Integer id){
+        User returnUser=repository.getOne(id);
+        List<Post>postList=returnUser.getPosts();
+        for (Post p:postList){
+            p.setVoteCount(voteRepository.countVotesByPostId(p.getId()))
+        }
+        return returnUser;
+    }
 }
