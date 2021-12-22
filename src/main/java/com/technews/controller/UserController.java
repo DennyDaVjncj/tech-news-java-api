199db5fbdd4a0bb0423647bbdd647a2c6e8283f4
Denny da Vjncj <daVjncjCode@gmail.com>
2021-12-21 20:51:20 -0500

    autowired annotaion, mapping to user table configured

--- src/main/java/com/technews/controller/UserController.java
+++ src/main/java/com/technews/controller/UserController.java
@@ -1,12 +1,21 @@
 package com.technews.controller;
 
 import com.technews.repository.UserRepository;
+import com.technews.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
     @Autowired
     UserRepository repository;
+    @Autowired
+    VoteRepository voteRepository;
+
+    @GetMapping("/api/users")
+    public List<User> getAllUsers(){
+        List<User> userList=Repository.findAll();
+    }
 
 }
