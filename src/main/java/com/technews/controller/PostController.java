a3d98afab7d87ab5433dacfdf1c9f21fe85227c4
Denny da Vjncj <daVjncjCode@gmail.com>
2021-12-27 21:57:53 -0500

    configuring the handling of requests to and from database

--- src/main/java/com/technews/controller/PostController.java
+++ src/main/java/com/technews/controller/PostController.java
@@ -8,6 +8,51 @@ import com.technews.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
+@RestController
 public class PostController {
+    @Autowired
+    PostRepository repository;
+    @Autowired
+    VoteRepository voteRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/api/posts")
+    public List<Post>getAllPosts(){
+        List<Post>postList=repository.findAll();
+        for(Post p:postList){
+            p.setVoteCount(voteRepository.countVotesByPostId(p.getId()));
+        }
+        return postList;
+    }
+    @GetMapping("/api/posts/{id}")
+    public Post getPost(@PathVariable Integer id){
+        Post returnPost=repository.getOne(id);
+        returnPost.setVoteCount(voteRepository.countVotesByPostId(returnPost.getId()));
+        return returnPost;
+    }
+    @PostMapping("/api/posts")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Post addPost(@RequestBody Post post){
+        repository.save(post);
+        return post;
+    }
+    @PutMapping("/api/posts/{id}")
+    public Post updatePost(@PathVariable int id,@RequestBody Post post){
+        Post tempPost=repository.getOne(id);
+        tempPost.setTitle(post.getTitle());
+        return repository.save(tempPost);
+    }
+    @PutMapping("/api/posts/upvote")
+    public String addVote(@RequestBody Vote vote,HttpServletRequest request){
+        String returnValue="";
+
+        if(request.getSession(fale)==null){
+            Post returnPost=null;
+        }
+    }
+
 }
