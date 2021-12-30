d2b830a046c9744fb93424b8ae3ebb65c7e884e4
Denny da Vjncj <daVjncjCode@gmail.com>
2021-12-30 15:15:42 -0500

    retrieval functionality configured

--- src/main/java/com/technews/controller/CommentController.java
+++ src/main/java/com/technews/controller/CommentController.java
@@ -1,8 +1,23 @@
 package com.technews.controller;
-
+import com.technews.model.Comment;
+import com.technews.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class CommentController {
+    @Autowired
+    CommentRepository repository;//reference type. new instance of comment repo
 
+    @GetMapping("/api/comments")
+    public List<Comment> getAllComments(){
+        return repository.findAll();//gets all comments sored within database
+    }
+    @GetMapping("/api/comments/{id}")
+    public Comment getComment(@PathVariable int id){
+        return repository.getOne(id);
+    }
 }
